5d9d165cec7ff6419d92e4df2ba11f64ed71b52e
Syeda Iman Sarwar <69895631+imansarwar@users.noreply.github.com>
2021-03-05 12:37:12 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import react from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import ToDoList from "./ToDoList";
+
+ReactDOM.render(<ToDoList />, document.getElementById("root"));
